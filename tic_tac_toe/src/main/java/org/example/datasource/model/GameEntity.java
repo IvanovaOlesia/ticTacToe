8438abf5e91afa8a3c26925ddb4192874f1b9619
Ivanova <Ivanova_olesia1993@mail.ru>
2025-02-01 00:06:15 +0700

    configuring authorization and changing a two-dimensional array to a one-dimensional one

--- tic_tac_toe/src/main/java/org/example/datasource/model/GameEntity.java
+++ tic_tac_toe/src/main/java/org/example/datasource/model/GameEntity.java
@@ -1,13 +1,13 @@
 package org.example.datasource.model;
 
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+
 import jakarta.persistence.*;
 import lombok.*;
 import lombok.Data;
 import org.hibernate.annotations.UuidGenerator;
 
-import java.util.Arrays;
+
 import java.util.UUID;
 
 
@@ -23,56 +23,7 @@ public class GameEntity {
     @GeneratedValue
     @UuidGenerator
     private UUID id;
-//    @Lob
-
-//
-//    private String boardJson;
-//    @Transient
-
     @Column(columnDefinition = "integer[]")
     private  int[] board;
-//    @Transient
-//    private static final ObjectMapper objectMapper = new ObjectMapper();
-//    @PostLoad
-//    private void initBoard() {
-//        if (boardJson != null) {
-//            try {
-//                board = objectMapper.readValue(boardJson, int[][].class);
-//            } catch (Exception e) {
-//                e.printStackTrace();
-//            }
-//        }
-//    }
-
-//public GameEntity(UUID id, int[][] board) {
-//    this.id = id;
-//    this.board = board;
-//    try {
-//        boardJson = objectMapper.writeValueAsString(board);
-//    } catch (Exception e) {
-//        e.printStackTrace();
-//    }
-//}
-//    public int[][] getBoard() {
-//        if (boardJson != null && board == null) {
-//            try {
-//                board = objectMapper.readValue(boardJson, int[][].class);
-//            } catch (Exception e) {
-//                e.printStackTrace();
-//            }
-//        }
-//        return board;
-//    }
-//
-//    public void setBoard(int[][] board) {
-//        this.board = board;
-//        try {
-//            boardJson = objectMapper.writeValueAsString(board);
-//
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-//    }
-
 
 }
--- tic_tac_toe/src/main/java/org/example/web/mapper/GameMapperDTO.java
+++ tic_tac_toe/src/main/java/org/example/web/mapper/GameMapperDTO.java
@@ -6,13 +6,11 @@ import org.example.web.dto.GameDTO;
 public class GameMapperDTO {
     public static GameDTO toDTO(Game game){
         return GameDTO.builder()
-//                .id(game.getId())
                 .board(game.getBoard())
                 .build();
     }
     public static Game fromDTO(GameDTO game){
         return Game.builder()
-//                .id(game.getId())
                 .board(game.getBoard())
                 .build();
     }
